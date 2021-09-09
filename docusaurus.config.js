c7b6add5988a8fb852958957b1d19da7b1db1d71
jeremytomlinson <jeremy.tomlinson@gmail.com>
2021-09-08 18:11:12 -0600

    Updated header links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ module.exports = {
       },
       items: [
         {
-          href: '/',
+          href: 'https://www.iot-ensemble.com',
           label: 'Home',
           position: 'left',
           target: '_top',
